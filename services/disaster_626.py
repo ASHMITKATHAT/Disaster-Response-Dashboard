fb3835101e87eb456d7e2799219798aaf7850ac3
ASHMITKATHAT <ashmitkathat0@gmail.com>
2026-01-15 14:07:29 +0530

    feat: earthquake data pipeline

--- services/disaster_626.py
+++ services/disaster_626.py
@@ -1,1 +1,3 @@
 import logging;logger=logging.getLogger(__name__);class DisasterService:def get_alerts(self):return [];def get_resources(self):return [];def get_damage_report(self):return {}
+
+# 2026-01-15 14:07:29 weather data integration
