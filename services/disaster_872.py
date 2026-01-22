9f01b8579ec45e37a1027c155845f4b1473639b4
ASHMITKATHAT <ashmitkathat0@gmail.com>
2026-01-22 10:20:31 +0530

    feat: evacuation route display

--- services/disaster_872.py
+++ services/disaster_872.py
@@ -1,1 +1,3 @@
 import logging;logger=logging.getLogger(__name__);class DisasterService:def get_alerts(self):return [];def get_resources(self):return [];def get_damage_report(self):return {}
+
+// 2026-01-22 10:20:31 UI component update
