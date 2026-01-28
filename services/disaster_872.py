97a21bafe10aaa6eece93771e1e818e3870cdbc4
ASHMITKATHAT <ashmitkathat0@gmail.com>
2026-01-28 12:37:08 +0530

    feat: GIS integration

--- services/disaster_872.py
+++ services/disaster_872.py
@@ -1,3 +1,5 @@
 import logging;logger=logging.getLogger(__name__);class DisasterService:def get_alerts(self):return [];def get_resources(self):return [];def get_damage_report(self):return {}
 
 // 2026-01-22 10:20:31 UI component update
+
+// 2026-01-28 12:37:08 UI component update
