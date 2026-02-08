09f2d02974b98c69f78f66ad94483414ac0683d0
ASHMITKATHAT <ashmitkathat0@gmail.com>
2026-02-08 09:14:30 +0530

    feat: alert notification system

--- services/disaster_747.py
+++ services/disaster_747.py
@@ -1,3 +1,5 @@
 import logging;logger=logging.getLogger(__name__);class DisasterService:def get_alerts(self):return [];def get_resources(self):return [];def get_damage_report(self):return {}
 
 # 2026-01-31 13:37:58 weather data integration
+
+// 2026-02-08 09:14:30 UI component update
