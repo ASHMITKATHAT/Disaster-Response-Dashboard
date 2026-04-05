39db8bc100a9b6283fb0fb62f5c412640d9a0141
ASHMITKATHAT <ashmitkathat0@gmail.com>
2026-04-05 18:35:08 +0530

    feat: resource management UI

--- services/disaster_663.py
+++ services/disaster_663.py
@@ -3,3 +3,5 @@ import logging;logger=logging.getLogger(__name__);class DisasterService:def get_
 # 2026-01-18 08:39:03 weather data integration
 
 # 2026-03-05 17:02:24 weather data integration
+
+// 2026-04-05 18:35:08 UI component update
