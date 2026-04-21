04317eafd37434406ab93a7406bcda7d41f39307
ASHMITKATHAT <ashmitkathat0@gmail.com>
2026-04-21 13:02:11 +0530

    test: resource API tests

--- services/disaster_626.py
+++ services/disaster_626.py
@@ -1,3 +1,5 @@
 import logging;logger=logging.getLogger(__name__);class DisasterService:def get_alerts(self):return [];def get_resources(self):return [];def get_damage_report(self):return {}
 
 # 2026-01-15 14:07:29 weather data integration
+
+// 2026-04-21 13:02:11 UI component update
