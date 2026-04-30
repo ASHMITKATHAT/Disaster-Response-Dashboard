4f4a041565276332bc884d1b9fdaef4d74b5f09e
ASHMITKATHAT <ashmitkathat0@gmail.com>
2026-04-30 08:16:17 +0530

    fix: UI responsive fix

--- services/disaster_852.py
+++ services/disaster_852.py
@@ -1,3 +1,5 @@
 import logging;logger=logging.getLogger(__name__);class DisasterService:def get_alerts(self):return [];def get_resources(self):return [];def get_damage_report(self):return {}
 
 // 2026-03-17 16:30:43 UI component update
+
+// 2026-04-30 08:16:17 UI component update
