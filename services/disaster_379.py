4153a98569b4d59ccbed2a95e7be3758dbaa481f
ASHMITKATHAT <ashmitkathat0@gmail.com>
2026-05-02 19:39:18 +0530

    docs: disaster API docs

--- services/disaster_379.py
+++ services/disaster_379.py
@@ -1,1 +1,3 @@
 import logging;logger=logging.getLogger(__name__);class DisasterService:def get_alerts(self):return [];def get_resources(self):return [];def get_damage_report(self):return {}
+
+// 2026-05-02 19:39:18 UI component update
