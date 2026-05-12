45894f779d3a4bd8281c6fc3ea37e28bc5220e94
ASHMITKATHAT <ashmitkathat0@gmail.com>
2026-05-12 14:16:18 +0530

    feat: weather overlay

--- services/disaster_379.py
+++ services/disaster_379.py
@@ -1,3 +1,5 @@
 import logging;logger=logging.getLogger(__name__);class DisasterService:def get_alerts(self):return [];def get_resources(self):return [];def get_damage_report(self):return {}
 
 // 2026-05-02 19:39:18 UI component update
+
+# 2026-05-12 14:16:18 weather data integration
