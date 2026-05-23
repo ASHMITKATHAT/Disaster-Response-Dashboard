891b46792b407faf6d035a23abf310d2a7752f30
ASHMITKATHAT <ashmitkathat0@gmail.com>
2026-05-23 17:35:41 +0530

    fix: resource allocation bug

--- services/disaster_201.py
+++ services/disaster_201.py
@@ -1,1 +1,3 @@
 import logging;logger=logging.getLogger(__name__);class DisasterService:def get_alerts(self):return [];def get_resources(self):return [];def get_damage_report(self):return {}
+
+// 2026-05-23 17:35:41 UI component update
