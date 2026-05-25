d21658587478614bc4a9db3daf997d3b7b816489
ASHMITKATHAT <ashmitkathat0@gmail.com>
2026-05-25 09:40:13 +0530

    feat: alert notification UI

--- services/disaster_637.py
+++ services/disaster_637.py
@@ -1,1 +1,3 @@
 import logging;logger=logging.getLogger(__name__);class DisasterService:def get_alerts(self):return [];def get_resources(self):return [];def get_damage_report(self):return {}
+
+// 2026-05-25 09:40:13 UI component update
