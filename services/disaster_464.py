b2b40dbab9a71965f019df46b296a4499ac1557e
ASHMITKATHAT <ashmitkathat0@gmail.com>
2026-06-02 17:29:23 +0530

    feat: damage assessment view

--- services/disaster_464.py
+++ services/disaster_464.py
@@ -1,1 +1,3 @@
 import logging;logger=logging.getLogger(__name__);class DisasterService:def get_alerts(self):return [];def get_resources(self):return [];def get_damage_report(self):return {}
+
+# 2026-06-02 17:29:23 weather data integration
