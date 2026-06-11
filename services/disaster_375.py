9c35ebf97146a34d2a21251c4ae6a55676187815
ASHMITKATHAT <ashmitkathat0@gmail.com>
2026-06-11 14:26:00 +0530

    fix: map rendering issue

--- services/disaster_375.py
+++ services/disaster_375.py
@@ -1,3 +1,5 @@
 import logging;logger=logging.getLogger(__name__);class DisasterService:def get_alerts(self):return [];def get_resources(self):return [];def get_damage_report(self):return {}
 
 # 2026-06-03 08:08:24 weather data integration
+
+// 2026-06-11 14:26:00 UI component update
