bfdff0656a9b1cf10cbb4af1b6d61c6ce56be1ab
ASHMITKATHAT <ashmitkathat0@gmail.com>
2026-06-13 10:23:28 +0530

    test: alert system tests

--- services/disaster_970.py
+++ services/disaster_970.py
@@ -1,3 +1,5 @@
 import logging;logger=logging.getLogger(__name__);class DisasterService:def get_alerts(self):return [];def get_resources(self):return [];def get_damage_report(self):return {}
 
 # 2026-05-05 19:12:47 weather data integration
+
+// 2026-06-13 10:23:28 UI component update
