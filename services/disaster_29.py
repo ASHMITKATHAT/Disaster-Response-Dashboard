53eddff441508dfe8ab60f073deb33443ccc0898
ASHMITKATHAT <ashmitkathat0@gmail.com>
2026-06-13 17:31:03 +0530

    fix: map rendering issue

--- services/disaster_29.py
+++ services/disaster_29.py
@@ -1,1 +1,3 @@
 import logging;logger=logging.getLogger(__name__);class DisasterService:def get_alerts(self):return [];def get_resources(self):return [];def get_damage_report(self):return {}
+
+// 2026-06-13 17:31:03 UI component update
