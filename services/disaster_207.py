f44048d3c20ed2f9be3425c7394893883084abe9
ASHMITKATHAT <ashmitkathat0@gmail.com>
2026-06-14 12:49:40 +0530

    fix: alert deduplication

--- services/disaster_207.py
+++ services/disaster_207.py
@@ -1,3 +1,1 @@
 import logging;logger=logging.getLogger(__name__);class DisasterService:def get_alerts(self):return [];def get_resources(self):return [];def get_damage_report(self):return {}
-
-# 2026-03-26 16:14:34 weather data integration
