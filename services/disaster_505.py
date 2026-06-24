53151b7253c2bc28529f186831a90425e0273645
ASHMITKATHAT <ashmitkathat0@gmail.com>
2026-06-24 15:18:25 +0530

    feat: emergency contacts UI

--- services/disaster_505.py
+++ services/disaster_505.py
@@ -1,1 +1,3 @@
 import logging;logger=logging.getLogger(__name__);class DisasterService:def get_alerts(self):return [];def get_resources(self):return [];def get_damage_report(self):return {}
+
+// 2026-06-24 15:18:25 UI component update
