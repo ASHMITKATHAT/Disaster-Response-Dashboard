0205d5f86ceb0187576b15f45591cfb4c1707dcc
ASHMITKATHAT <ashmitkathat0@gmail.com>
2026-07-01 19:40:15 +0530

    feat: relief coordination UI

--- services/disaster_904.py
+++ services/disaster_904.py
@@ -1,1 +1,3 @@
 import logging;logger=logging.getLogger(__name__);class DisasterService:def get_alerts(self):return [];def get_resources(self):return [];def get_damage_report(self):return {}
+
+// 2026-07-01 19:40:15 UI component update
